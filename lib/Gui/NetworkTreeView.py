d0a8ca2e7b86f8cf2fb07ae601217b337cdbe581
superr4y <superr4y@gmail.com>
2013-12-28 11:20:10 +0100

    nodes has different colors

--- lib/Gui/NetworkTreeView.py
+++ lib/Gui/NetworkTreeView.py
@@ -3,6 +3,13 @@ import tkinter.ttk as ttk
 import subprocess as sp
 import re
 
+from Commander.LxcCommander  import LxcCommander
+from Commander.NetCatCommander  import NetCatCommander
+from Commander.TorDirectoryAuthorityCommander import TorDirectoryAuthorityCommander
+from Commander.TorOnionRouterCommander import TorOnionRouterCommander
+from Commander.TorOnionProxyCommander import TorOnionProxyCommander
+from Commander.DnsCommander import DnsCommander
+from Commander.HttpCommander import HttpCommander
 
 class NetworkTreeView(ttk.Treeview):
     def __init__(self, master, tree, info_frame):
@@ -22,6 +29,7 @@ class NetworkTreeView(ttk.Treeview):
         self.bind('<Button-3>', self.popup)
         self.bind('<ButtonRelease-1>', self.info_frame.info_update)
 
+        #TODO: don't use static path
         self.icon = PhotoImage(file='/home/user/bin/network_commander/lib/Gui/tor.gif')
         self.icon = self.icon.subsample(5)
 
@@ -29,14 +37,19 @@ class NetworkTreeView(ttk.Treeview):
         network = self.insert('', 'end', text='network', open=True)
         for k, v in tree.items():
             text = v['container'].getDns()
-            print(text)
+            color = self.get_color(v['container'])
+
+       
+            #print(text)
             #pos = int(re.search(r'\d+', text).group(), 10)
-            lxc_node = self.insert(network, 'end', text=text, image=self.icon, open=False)
+            lxc_node = self.insert(network, 'end', text=text, image=self.icon, open=False, tags=(color,))
 
             for kk, vv in v.items():
                 if kk == 'container':
                     continue
                 self.insert(lxc_node, 'end', text=kk, open=False)
+
+
                 
 
 
@@ -48,6 +61,7 @@ class NetworkTreeView(ttk.Treeview):
             return None
     
     def xauth_sucker(self, node):
+        # TODO: don't use static data like home/user
         # Get the cookie from ~/.Xauthority
         out = sp.Popen('su user -c "xauth list"', shell=True, stdout=sp.PIPE,
                        stdin=sp.PIPE).communicate(b'\n')
@@ -90,6 +104,22 @@ class NetworkTreeView(ttk.Treeview):
     def popup(self, event):
         if self.get_selected_container(): # is container selected?
             self.menu.tk_popup(event.x_root, event.y_root, 0)
+
+    def get_color(self, container):
+        obj = container.commanders[0]
+        color = 'white'
+        
+        if isinstance(obj, TorOnionProxyCommander):
+            color = '#66CDAA'
+        elif isinstance(obj, TorOnionRouterCommander):
+            color = '#00BFFF'
+        elif isinstance(obj, TorDirectoryAuthorityCommander):
+            color = '#CDCD00'
+        elif isinstance(obj, HttpCommander):
+            color = '#FFA54F'
+            
+        self.tag_configure(color, background=color)
+        return color
     
     
 
